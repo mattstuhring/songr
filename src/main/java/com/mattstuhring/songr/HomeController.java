932a844a8762f9feee565293aa75bff82beda6d5
Matt Stuhring <whereyouatmatt@gmail.com>
2019-08-20 11:55:17 -0700

    Completed lab-11

---
+++ src/main/java/com/mattstuhring/songr/HomeController.java
@@ -0,0 +1,44 @@
+package com.mattstuhring.songr;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class HomeController {
+
+    @GetMapping("/hello")
+    public String getRoot() {
+        System.out.println("Somebody got the root");
+        return "helloworld";
+    }
+
+    @GetMapping("/capitalize/{words}")
+    public String getCapitalizeWords(@PathVariable String words, Model m) {
+        m.addAttribute("words", capitalizeWords(words));
+        return "capitalize";
+    }
+
+    @GetMapping("/reverse")
+    public String getReversedWords(@RequestParam(required = false, defaultValue = "I like to program in Java") String words, Model m) {
+        m.addAttribute("reversed", reverseOrderOfWords(words));
+        return "reversed";
+    }
+
+    public static String capitalizeWords(String words) {
+        return words.toUpperCase();
+    }
+
+    public static String reverseOrderOfWords(String words) {
+        String[] wordArr = words.split(" ");
+        StringBuilder reversedWords = new StringBuilder();
+
+        for (String w : wordArr) {
+            reversedWords.insert(0, w + " ");
+        }
+
+        return reversedWords.toString().trim();
+    }
+}
